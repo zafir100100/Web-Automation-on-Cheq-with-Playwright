ae29b7b7d1f6f6bc26c6c04772128a3cee01530f
zafir100100 <55599023+zafir100100@users.noreply.github.com>
2025-03-16 19:54:30 +0600

    order suite completed

--- pages/PreOrderPage.js
+++ pages/PreOrderPage.js
@@ -1,3 +1,5 @@
+const { expect } = require('@playwright/test');
+
 class PreOrderPage {
     constructor(page) {
         this.page = page;
@@ -5,24 +7,36 @@ class PreOrderPage {
         this.next_button = page.locator("//*[@data-testid='KeyboardArrowRightIcon']");
         this.add_to_order_button = page.locator("//button[text()='+ Add to order']");
         this.submit_pre_order_button = page.locator("//button[@data-test-id='view-cart-btn']");
+        this.my_order_label = page.getByText(/my order/i);
     }
 
     async addItems() {
-        const elements = await this.category_labels.elementHandles();
-        for (const element of elements) {
-            if (!(await element.isVisible())) {
+        const categoryCount = await this.category_labels.count();
+        for (let i = 0; i < categoryCount; i++) {
+            let categories = await this.category_labels.elementHandles();
+            let category = categories[i];
+            if (!(await category.isVisible())) {
                 await this.next_button.click();
+                categories = await this.category_labels.elementHandles();
+                category = categories[i];
             }
-            await element.click();
+            await category.click();
             await this.page.keyboard.press('Tab');
             await this.page.keyboard.press('Tab');
             await this.page.keyboard.press('Enter');
             await this.add_to_order_button.click();
+            await this.add_to_order_button.waitFor({ state: 'hidden', timeout: 40000 });
+            break;
         }
     }
 
     async submitPreOrder() {
         await this.submit_pre_order_button.click();
+    }
+
+    async validatePreOrderSubmission() {
+        await expect(this.submit_pre_order_button).toBeVisible({ state: 'hidden' });
+        await expect(this.my_order_label).toBeVisible();
     }
 }
 
---
+++ tests/OrderTestRunner.spec.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const { test } = require('@playwright/test');
+const allure = require('allure-playwright');
+
+const LoginPage = require('../pages/LoginPage');
+const MyEventPage = require('../pages/MyEventPage');
+const MySuitePage = require('../pages/MySuitesPage');
+const SuitePreferencesPage = require('../pages/SuitePreferencesPage');
+const PreOrderPage = require('../pages/PreOrderPage');
+const MyOrderPage = require('../pages/MyOrderPage');
+
+test.describe('Order', () => {
+    test('@order @regression Product order with payment', async ({ page }) => {
+        await allure.description("This test checks the full product order flow, including login, event submission, suite preferences, pre-order items, and final payment.");
+        await allure.epic("Product Order Flow");
+        await allure.feature("Order Product with Payment");
+        await allure.story("As a user, I want to order a product and make a payment successfully.");
+        await allure.tags("order", "regression", "payment", "ui");
+        await allure.owner("zafir100100");
+
+        const loginPage = new LoginPage(page);
+        await loginPage.gotoLoginPage();
+        await loginPage.doLogin(process.env.LOGIN_EMAIL, process.env.LOGIN_PASSWORD);
+        await loginPage.validateSuccessfulLogin();
+        const myEventPage = new MyEventPage(page);
+        await myEventPage.submitEvent();
+        await myEventPage.validateSuccessfulEventSubmission();
+        const mySuitesPage = new MySuitePage(page);
+        const suiteState = await mySuitesPage.submitSuite();
+        if (suiteState === 1) {
+            const suitePreferencesPage = new SuitePreferencesPage(page);
+            await suitePreferencesPage.submitSuitePreferences();
+            await suitePreferencesPage.validateSuitePreferencesSubmission();
+        }
+        const preOrderPage = new PreOrderPage(page);
+        await preOrderPage.addItems();
+        await preOrderPage.submitPreOrder();
+        await preOrderPage.validatePreOrderSubmission();
+        const myOrderPage = new MyOrderPage(page);
+        await myOrderPage.submitPreOrder();
+        await myOrderPage.submitCreditCardInfo(process.env.CREDIT_CARD_NUMBER, process.env.CREDIT_CARD_EXPIRY, process.env.CREDIT_CARD_CVV, process.env.CREDIT_CARD_NAME);
+        await myOrderPage.validateOrderSubmission();
+    });
+
+    // test('@order Pre-Order product add to Cart', async ({ page }) => {
+    //     const loginPage = new LoginPage(page);
+    //     await loginPage.gotoLoginPage();
+    //     await loginPage.doLogin(process.env.LOGIN_EMAIL, process.env.LOGIN_PASSWORD);
+    //     await loginPage.validateSuccessfulLogin();
+    //     const myEventPage = new MyEventPage(page);
+    //     await myEventPage.submitEvent();
+    //     await myEventPage.validateSuccessfulEventSubmission();
+    //     const mySuitesPage = new MySuitePage(page);
+    //     const suiteState = await mySuitesPage.submitSuite();
+    //     if (suiteState === 1) {
+    //         const suitePreferencesPage = new SuitePreferencesPage(page);
+    //         await suitePreferencesPage.submitSuitePreferences();
+    //         await suitePreferencesPage.validateSuitePreferencesSubmission();
+    //     }
+    //     const preOrderPage = new PreOrderPage(page);
+    //     await preOrderPage.addItems();
+    //     await preOrderPage.submitPreOrder();
+    //     await preOrderPage.validatePreOrderSubmission();
+    // });
+});
